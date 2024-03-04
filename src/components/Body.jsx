521711c4cea70341b7a4cd4ef56f27a9ad096110
frustratedcat <brownjared205@gmail.com>
2024-03-04 11:58:13 -0500

    Add body components to body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,16 @@
+import Personal from "./body-components/Personal.jsx";
+import CV from "./body-components/CV.jsx";
+import Education from "./body-components/Education.jsx";
+import Experience from "./body-components/Experience.jsx";
+
 export default function Body() {
   return (
     <>
-      <section>button section</section>
       <main>
-        <section>form section</section>
-        <section>submit buttom section</section>
+        <Personal></Personal>
+        <Education></Education>
+        <Experience></Experience>
+        <CV></CV>
       </main>
     </>
   );
