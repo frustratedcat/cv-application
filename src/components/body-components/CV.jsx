a930bbd0cb4171d2a73154a2ae61d73cc1b90d7f
frustratedcat <brownjared205@gmail.com>
2024-03-04 17:09:30 -0500

    Add basic structure

--- src/components/body-components/CV.jsx
+++ src/components/body-components/CV.jsx
@@ -1,3 +1,20 @@
 export default function CV() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div className="cv-name-container"></div>
+        <div className="cv-email-container"></div>
+        <div className="cv-phone-container"></div>
+        <div className="cv-address-container"></div>
+      </section>
+      <section>
+        <h2>Education</h2>
+        <div className="cv-education-container"></div>
+      </section>
+      <section>
+        <h2>Experience</h2>
+        <div className="cv-experience-container"></div>
+      </section>
+    </>
+  );
 }
